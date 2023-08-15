4f4abaa83530c2b8e2654c55a85780429ac5aed2
Alex Rudolph <alex.rudolph@facefirst.com>
2023-08-15 15:49:48 -0700

    chore: remove -tzip from 7z

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ then
   cd $INPUT_DIRECTORY
 fi
 
-if [ "$INPUT_TYPE" = "zip" ] || [ "$INPUT_TYPE" = "7z" ] 
+if [ "$INPUT_TYPE" = "zip" ]
 then
   if [ "$RUNNER_OS" = "Windows" ]
   then
@@ -41,6 +41,28 @@ then
     else
       zip -r $INPUT_FILENAME $INPUT_PATH -x $INPUT_EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
     fi
+  fi
+elif [ "$INPUT_TYPE" = "7z" ]
+then
+  if [ -z "$INPUT_EXCLUSIONS" ] 
+  then
+    7z a $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+  else
+    EXCLUSIONS=''
+
+    for EXCLUSION in $INPUT_EXCLUSIONS
+    do
+      EXCLUSIONS+=" -x!"
+      EXCLUSIONS+=$EXCLUSION
+    done
+
+    for EXCLUSION in $INPUT_RECURSIVE_EXCLUSIONS
+    do
+      EXCLUSIONS+=" -xr!"
+      EXCLUSIONS+=$EXCLUSION
+    done
+
+    7z a $INPUT_FILENAME $INPUT_PATH $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
   fi
 elif [ "$INPUT_TYPE" = "tar" ] || [ "$INPUT_TYPE" = "tar.gz" ] 
 then
