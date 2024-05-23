8f2cab35cc6a1a65250e3286e083d64346eca43a
AR-FF <135644784+AR-FF@users.noreply.github.com>
2024-05-23 12:12:25 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,6 @@ set -eu
 
 printf "\n📦 Creating %s archive...\n" "$INPUT_TYPE"
 
-if [ -n "$INPUT_COMMAND" ]
-then
-  eval $INPUT_COMMAND
-fi
-
 if [ "$INPUT_DIRECTORY" != "." ] 
 then
   cd "$INPUT_DIRECTORY"
